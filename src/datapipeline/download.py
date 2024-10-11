12c9304e761b0dbbc6caddb56369d1d2c153e63a
Mingyuan Ma <mingyuan_ma@g.harvard.edu>
2024-10-11 00:19:02 -0400

    hi

--- src/datapipeline/download.py
+++ src/datapipeline/download.py
@@ -16,12 +16,12 @@ def download_file_from_gcs(bucket_name, source_blob_name, destination_file_name)
 
 
 if __name__ == "__main__":
-    LOCAL_DIR = "datasets"
+    LOCAL_DIR = "dataset"
     bucket_name = "ac215-reddit-finance-data"
     source_blob_name = "reddit-processed/train/reddit_1k.jsonl"
     if not os.path.exists(LOCAL_DIR):
         os.makedirs(LOCAL_DIR)
-    destination_file_name = f"{LOCAL_DIR}/hi.jsonl"
+    destination_file_name = f"{LOCAL_DIR}/reddit_1k.jsonl"
     download_file_from_gcs(
         bucket_name=bucket_name,
         source_blob_name=source_blob_name,
