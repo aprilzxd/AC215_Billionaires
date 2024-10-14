aee855b3c7c1203fa433b18298c6807932433425
Mingyuan Ma <mingyuan_ma@g.harvard.edu>
2024-10-14 14:16:24 -0400

    add agents

--- src/datapipeline/docker-shell.sh
+++ src/datapipeline/docker-shell.sh
@@ -1,7 +1,7 @@
 docker build -t datapipeline -f Dockerfile .
 
 docker run --rm -ti \
-  -v "$(pwd)/../../../secrets:/secrets" \
+  -v "$(pwd)/../../secrets:/secrets" \
   -v "$(pwd)/../../dataset:/app/dataset" \
   -e GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json" \
   datapipeline:latest
