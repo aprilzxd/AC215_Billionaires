1a0d10853174dbadfc0d12aeecf5c71df804a1e9
aprilzxd <77734302+aprilzxd@users.noreply.github.com>
2024-10-18 14:20:56 -0400

    Revert "Milestone2"

--- src/agents/docker-shell.sh
+++ src/agents/docker-shell.sh
@@ -1,3 +1,1 @@
-docker build -t agents -f Dockerfile .
-
 docker run --rm -it -e OPENAI_API_KEY=your_openai_api_key -p 8501:8501 agents
--- src/datapipeline/dataloader.py
+++ src/datapipeline/dataloader.py
@@ -1,28 +1,4 @@
-#! Under construction!
-import os
-from google.cloud import storage
+import pandas as pd
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../secrets/llm-service-account.json"
-
-def download_file_from_gcs(bucket_name, source_blob_name, destination_file_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
-    blob.download_to_filename(destination_file_name)
-    print(
-        f"Downloaded {source_blob_name} from bucket {bucket_name} to {destination_file_name}."
-    )
-
-
-if __name__ == "__main__":
-    LOCAL_DIR = "dataset"
-    bucket_name = "ac215-reddit-finance-data"
-    source_blob_name = "reddit-processed/train/reddit_1k.jsonl"
-    if not os.path.exists(LOCAL_DIR):
-        os.makedirs(LOCAL_DIR)
-    destination_file_name = f"{LOCAL_DIR}/reddit_1k_testing.jsonl"
-    download_file_from_gcs(
-        bucket_name=bucket_name,
-        source_blob_name=source_blob_name,
-        destination_file_name=destination_file_name,
-    )
+df = pd.read_json("hf://datasets/winddude/reddit_finance_43_250k/top.jsonl", lines=True)
+df.to_json("dataset/top.jsonl", orient='records', lines=True)
