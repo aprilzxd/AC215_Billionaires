24ace8ba062f7f872711c013cc8ae98900422807
Lance Lu <77723432+Lancelottery@users.noreply.github.com>
2024-12-11 20:38:13 -0500

    Delete z_srcipts.sh

--- z_srcipts.sh
+++
@@ -1,8 +0,0 @@
-docker build -t mamingyuan2001/billionaire-api-service:v12 --platform=linux/amd64/v2 -f Dockerfile .
-docker push mamingyuan2001/billionaire-api-service:v12
-
-docker build -t mamingyuan2001/billionaire-frontend:v12 --platform=linux/amd64/v2 -f Dockerfile .
-docker push mamingyuan2001/billionaire-frontend:v12
-
-gcloud container clusters create test-cluster --num-nodes 2 --zone us-east1-c --machine-type n2d-standard-2
-
