016f40c9799f383dca9ffaf36205f8d4ddd28af4
Lance Lu <77723432+Lancelottery@users.noreply.github.com>
2024-12-11 23:04:36 -0500

    test

--- test-runner/docker-shell.sh
+++ test-runner/docker-shell.sh
@@ -3,7 +3,6 @@ set -e
 
 export SECRETS_DIR=$(pwd)/../../secrets/
 if [ ! -f "${SECRETS_DIR}/openai_key.txt" ]; then
-  echo "Error: openai_key.txt not found in ${SECRETS_DIR}!"
   exit 1
 fi
 
@@ -12,4 +11,4 @@ export OPENAI_API_KEY=$(cat ${SECRETS_DIR}/openai_key.txt)
 docker-compose up --build -d
 
 echo "Containers are starting..."
-echo "OPENAI_API_KEY is loaded from ${SECRETS_DIR}/openai_key.txt"
+echo "OPENAI_API_KEY is loaded from ${SECRETS_DIR}openai_key.txt"
